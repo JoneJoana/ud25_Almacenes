d5185bda1e8aa5810f007761c9c0623c7bfefc18
Joana <joanajanefdez@gmail.com>
2022-08-17 20:02:49 +0200

    DAO

--- src/main/java/com/ud25_Almacenes/dao/IAlmacenesDAO.java
+++ src/main/java/com/ud25_Almacenes/dao/IAlmacenesDAO.java
@@ -1,5 +1,10 @@
 package com.ud25_Almacenes.dao;
 
-public interface IAlmacenesDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ud25_Almacenes.dto.Almacenes;
+
+
+public interface IAlmacenesDAO extends JpaRepository<Almacenes,Integer>{
 
 }
--- src/main/java/com/ud25_Almacenes/dao/ICajasDAO.java
+++ src/main/java/com/ud25_Almacenes/dao/ICajasDAO.java
@@ -1,5 +1,17 @@
 package com.ud25_Almacenes.dao;
 
-public interface ICajasDAO {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ud25_Almacenes.dto.Cajas;
+
+
+public interface ICajasDAO extends JpaRepository<Cajas,String>{
+
+	public Cajas findByNumRef(String numRef);
+	
+	public List<Cajas> findByAlmacen(int idAlmacen);
+	
+	public void deleteCaja(String numRef);	
+	
 }
