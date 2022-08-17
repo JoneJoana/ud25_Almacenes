08220342b223b335b6a4ed2063a80ac17bb6a6ab
Joana <joanajanefdez@gmail.com>
2022-08-17 11:28:25 +0200

    config inicial

---
+++ src/test/java/com/ud25_Almacenes/Ud25AlmacenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ud25_Almacenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud25AlmacenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
