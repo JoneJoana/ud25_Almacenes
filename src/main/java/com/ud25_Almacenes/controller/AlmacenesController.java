c704f43cb851f7c6b78f12c66549f250cad3e403
Joana <joanajanefdez@gmail.com>
2022-08-17 21:04:31 +0200

    controller

--- src/main/java/com/ud25_Almacenes/controller/AlmacenesController.java
+++ src/main/java/com/ud25_Almacenes/controller/AlmacenesController.java
@@ -1,5 +1,55 @@
 package com.ud25_Almacenes.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ud25_Almacenes.dto.Almacenes;
+import com.ud25_Almacenes.service.AlmacenesServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class AlmacenesController {
 
+	@Autowired
+	AlmacenesServiceImpl almacServImpl;
+	
+	@GetMapping("/almacenes")
+	public List<Almacenes> listAlmacenes(){
+		return almacServImpl.listAlmacenes();
+	}
+		
+	@GetMapping("/almacenes/{id}")
+	public Almacenes findById(@PathVariable(name="id") int id) {
+		return almacServImpl.findById(id);
+	}
+	
+	@DeleteMapping("/almacenes/{id}")
+	public void deleteAlmacen(@PathVariable(name="id") int id) {
+		almacServImpl.deleteAlmacen(id);
+	}
+	
+	@PostMapping("/almacenes")
+	public Almacenes saveAlmacen(Almacenes almacen) {
+		return almacServImpl.saveAlmacen(almacen);
+	}
+	
+	@PutMapping("/almacenes/{id}") //actualizar
+	public Almacenes updateAlmacen(@PathVariable(name="id") int id, @RequestBody Almacenes almacen) {
+		Almacenes almac_selec = almacServImpl.findById(id);
+		
+		almac_selec.setLugar(almacen.getLugar());
+		almac_selec.setCapacidad(almacen.getCapacidad());
+				
+		return almacServImpl.saveAlmacen(almac_selec);
+	}
+		
 }
--- src/main/java/com/ud25_Almacenes/controller/CajasController.java
+++ src/main/java/com/ud25_Almacenes/controller/CajasController.java
@@ -1,5 +1,74 @@
 package com.ud25_Almacenes.controller;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ud25_Almacenes.dto.Cajas;
+import com.ud25_Almacenes.service.CajasServiceImpl;
+
+@RestController
+@RequestMapping("/api")
 public class CajasController {
 
+	@Autowired
+	CajasServiceImpl cajasServiceImpl;
+	
+	@GetMapping("/cajas")
+	public List<Cajas> listCajas(){
+		return cajasServiceImpl.listCajas();
+	}
+	
+	@GetMapping("/cajas/{num_ref}")
+	public Cajas findByNumRef(@PathVariable(name="num_ref") String numRef) {
+		return cajasServiceImpl.findByNumRef(numRef);
+	}
+	
+	@GetMapping("/cajas/almacenes/{almacen}")
+	public List<Cajas> findByAlmacen(@PathVariable(name="almacen")int idAlmacen){
+		return cajasServiceImpl.findByAlmacen(idAlmacen);
+	}
+	
+	@DeleteMapping("/cajas/{num_ref}")
+	@Transactional
+	public void deleteCaja(@PathVariable(name="num_ref") String numRef) {
+		cajasServiceImpl.deleteCaja(numRef);
+	}
+	
+	@PostMapping("/cajas")
+	public String saveCaja(@RequestBody Cajas caja) {
+		boolean exists = false;
+		
+		for (Cajas c : cajasServiceImpl.listCajas()) {
+			if(c.getNum_ref().equals(caja.getNum_ref())) {
+				exists = true;
+			}
+		}
+		if(!exists) {
+			cajasServiceImpl.saveCaja(caja);
+			return "Caja "+ caja.getNum_ref()+" guardada!";
+		}
+		return "Caja existente";	
+	}
+	
+	@PutMapping("/cajas/{num_ref}")
+	public Cajas updateCaja(@PathVariable(name="num_ref") String numRef, @RequestBody Cajas caja) {
+		Cajas caja_selec = cajasServiceImpl.findByNumRef(numRef);
+		
+		caja_selec.setValor(caja.getValor());
+		caja_selec.setAlmacen(caja.getAlmacen());
+		
+		return cajasServiceImpl.saveCaja(caja_selec);
+	}
+	
 }
